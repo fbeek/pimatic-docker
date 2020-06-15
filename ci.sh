459380e0005ee84044df60db8c86aac0a6d1807e
florian.beek <AMDrult&061986abx>
2020-06-15 13:31:30 +0200

    travis test

--- ci.sh
+++ ci.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
-  docker buildx build \
-    --progress plain \
-    --platform=linux/amd64,linux/arm64,linux/arm/v7 \
-    .
-  exit $?
-fi
+
+docker buildx build \
+  --progress plain \
+  --platform=linux/amd64,linux/arm64,linux/arm/v7 \
+  .
+exit $?
