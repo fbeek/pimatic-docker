133818752714b6f3b56f0806a9ee04ad47f1fb72
florian.beek <AMDrult&061986abx>
2020-06-15 14:21:51 +0200

    test

--- ci.sh
+++ ci.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo $DOCKER_PASSWORD | docker login -u fbeek --password-stdin &> /dev/null
+echo
+echo $DOCKER_PASSWORD | docker login -u fbeek --password-stdin
 TAG="${TRAVIS_TAG:-latest}"
+echo "$DOCKER_PASSWORD $TAG"
 docker buildx build \
      --progress plain \
     --platform=linux/amd64,linux/arm64,linux/arm/v7 \
