daa005da4409e2924e62201efd1bd3f4460c8c6d
florian.beek <AMDrult&061986abx>
2020-06-15 13:49:29 +0200

    added multiarch build

--- ci.sh
+++ ci.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-
+echo $DOCKER_PASSWORD | docker login -u fbeek --password-stdin &> /dev/null
+TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
-  --progress plain \
-  --platform=linux/amd64,linux/arm64,linux/arm/v7 \
-  .
-exit $?
+     --progress plain \
+    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7 \
+    -t $DOCKER_REPO:$TAG \
+    --push \
+    .
