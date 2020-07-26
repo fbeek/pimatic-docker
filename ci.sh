c1069013b1f700c582a0571d95f68b11708ea049
Florian in der Beek <florian.inderbeek@tecbits.de>
2020-07-26 19:19:24 +0200

    changed build file

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@ echo $DOCKER_PASSWORD | docker login -u fbeek --password-stdin &> /dev/null
 TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
      --progress plain \
-    --platform=linux/amd64,linux/arm64,linux/arm/v7 \
+    --platform=linux/amd64,linux/arm/v7 \
     -t $DOCKER_REPO:$TAG \
     --push \
     .
